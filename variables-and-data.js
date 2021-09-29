924e62ebcde7f19fd5d0f6111cecc33a045999c5
Nick Del Toro <nickdt96@gmail.com>
2021-09-29 13:38:08 -0500

    Added variables-and-data.js, conditionals-and-functions.js and objects-apis.js files

---
+++ variables-and-data.js
@@ -0,0 +1,34 @@
+/*
+   Variables, Data Types, and Typing
+ */
+let wordData;
+wordData = "Word List";
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+let obj = {
+    key1: "Value 1",
+    key2: 3,
+    obj2: {
+        obj2key1: "value of inner object",
+    },
+};
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4/2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(string);
+
+
